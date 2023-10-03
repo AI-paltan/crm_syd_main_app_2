266391656c0c7fd55ce0be04a326540c55c90e8b
Jayesh Thukrul <jayesh.mgs35@gmail.com>
2023-10-03 15:12:27 +0530

    new template

--- data_dump_module/data_dump_config.py
+++ data_dump_module/data_dump_config.py
@@ -5,7 +5,7 @@ import os
 
 
 class DataDumpCoreSettings(BaseSettings):
-    cdm_template :str= os.path.join(path.dirname(__file__),'../assets/cdm_excel_template','CCIF_CDM_Input_Company name_date_Sydney_latest_template v2.0.xlsx')
+    cdm_template :str= os.path.join(path.dirname(__file__),'../assets/cdm_excel_template','CCIF_CDM_Input_Company name_date_Sydney_latest_template v3.0.xlsx')
     bs_breakdown_particular_colidx = 2
     pl_breakdown_particular_colidx = 2
     cdm_template_save_dir : str = os.path.join(path.dirname(__file__),"../CDM_Output")
